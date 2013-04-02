083ef109ebfd93c81b695effb1672434e9d26ac0
Jonathan Lim <snowblink@gmail.com>
2013-04-02 20:38:42 +0100

    Update env var

--- pollen.rb
+++ pollen.rb
@@ -20,7 +20,7 @@ Twitter.configure do |config|
   config.consumer_key       = ENV['CONSUMER_KEY']
   config.consumer_secret    = ENV['CONSUMER_SECRET']
   config.oauth_token        = ENV['OAUTH_TOKEN']
-  config.oauth_token_secret = ENV['OAUTH_SECRET']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
 end
 
 date = DateTime.now.strftime("%A %Y%m%d")
